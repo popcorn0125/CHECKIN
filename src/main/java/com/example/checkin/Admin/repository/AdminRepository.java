8d0d1f5d63be1b8b398815bd2d8afaab152ffbc2
김준식 <rlawnstlr54@gmail.com>
2025-11-11 21:14:04 +0900

    251111 김준식 관리자 로그인 기능 추가 완료

---
+++ src/main/java/com/example/checkin/Admin/repository/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.example.checkin.Admin.repository;
+
+import com.example.checkin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByUserId(String userId);
+
+
+
+}
